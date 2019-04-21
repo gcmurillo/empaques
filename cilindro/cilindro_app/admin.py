2abc70f79c279df5e9bf9a1516a59333b840f355
gcmurillo <gcmurillo@outlook.com>
2019-04-21 17:19:09 -0500

    Initial models

--- cilindro/cilindro_app/admin.py
+++ cilindro/cilindro_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Empresa)
+admin.site.register(Empleado_empresa)
+admin.site.register(Custodio)
 
 # Register your models here.
--- cilindro/cilindro_app/models.py
+++ cilindro/cilindro_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Empresa (models.Model):
+    nombre = models.CharField(max_length=20, help_text='Nombre de la empresa')
+    direccion = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=10)
+
+    def __str__(self):
+       return self.nombre
+
+
+class Empleado (models.Model):
+    nombre = models.CharField(max_length=20, help_text='Nombre del empleado')
+    cedula = models.CharField(max_length=10)
+    telefono = models.CharField(max_length=10)
+    empresa = models.ForeignKey(Empresa, on_delete=models.SET_NULL, blank=False, null=True)
+
+    def __str__(self):
+       return self.nombre
+
+
+class Custodio (models.Model):
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE,
+                                 blank=False, null=False, help_text='Representante de empresa externa')
+
+    def __str__(self):
+        return self.empleado.__str__() + ' ' + self.empleado.empresa
+
+
+class Ciudad (models.Model):
+    nombre = models.CharField(max_length=15, help_text='Nombre de la ciudad')
+    latitud = models.FloatField(null=True, blank=True, default=None)
+    longitud = models.FloatField(null=True, blank=True, default=None)
+
+    def __str__(self):
+        return self.nombre
+
+
